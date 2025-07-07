a43cf40b3123a7555aad07b9b0312aebd517ed25
northcape <northcape_sk@yahoo.co.jp>
2025-07-07 22:43:25 +0900

    A: Initial

---
+++ main.py
@@ -0,0 +1,14 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+import constants as C
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {C.SCREEN_WIDTH}")
+    print(f"Screen height: {C.SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
