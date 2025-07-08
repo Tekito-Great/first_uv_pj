2e212eaebfd2e1b82fb55bba04a06cb17d1b6a43
northcape <northcape_sk@yahoo.co.jp>
2025-07-08 20:25:14 +0900

    C: triangle drawn

--- main.py
+++ main.py
@@ -3,16 +3,26 @@
 # throughout this file
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+            
         screen.fill("black")
+        dt = clock.tick(60) / 1000
+        player.draw(screen)
         pygame.display.flip()
 
 
